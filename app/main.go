eeb283184bf65d51a4be05e405889e647c1baa8c
DiiVel <denkrapivnitsky@gmail.com>
2024-03-26 19:20:10 +0300

    full code in main.go need refactor to devide by files

--- app/main.go
+++ app/main.go
@@ -1,9 +1,74 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+	"weather-forecast-cli-tool/pkg"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	q := "Mumbai"
 
-	return
+	if len(os.Args) >= 2 {
+		q = os.Args[1]
+	}
+	res, err := http.Get("https://api.weatherapi.com/v1/forecast.json?key=1c35070f0d7648d9955100905231908&q=" + q + "&days=1&aqi=no&alerts=no")
+	if err != nil {
+		panic(err)
+	}
+	defer func(Body io.ReadCloser) {
+		if err := Body.Close(); err != nil {
+			panic(err)
+		}
+	}(res.Body)
+
+	if res.StatusCode != 200 {
+		panic("Weather API not available")
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var weather pkg.Weather
+
+	err = json.Unmarshal(body, &weather)
+	if err != nil {
+		panic(err)
+	}
+
+	location, current, hours := weather.Location, weather.Current, weather.Forecast.ForecastDay[0].Hour
+	fmt.Printf(
+		"%s, %s: %.0fC, %s\n",
+		location.Name,
+		location.Country,
+		current.TempC,
+		current.Condition.Text,
+	)
+
+	for _, hour := range hours {
+		date := time.Unix(hour.TimeEpoch, 0)
+
+		if date.Before(time.Now()) {
+			continue
+		}
+		message := fmt.Sprintf("%s - %.0fC, %.0f%%, %s\n",
+			date.Format("15:04"),
+			hour.TempC,
+			hour.ChanceOfRain,
+			hour.Condition.Text,
+		)
+		if hour.ChanceOfRain < 80 {
+			//color.Green(message)
+			fmt.Println(message)
+		} else {
+			//color.Red(message)
+			fmt.Println(message)
+		}
+	}
 }
